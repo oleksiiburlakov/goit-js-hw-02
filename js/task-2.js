9177db1a84f1c30283be57f3a0544cd63b65429f
oleksiiburlakov <oleksii.burlakov1987@gmail.com>
2024-12-08 15:58:52 +0100

    add text

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function formatMessage(message, maxLength){
+    let newMessage;
+    if(message.length <= maxLength){
+        return message;
+    } else {
+        newMessage = message.slice(0, maxLength) + "...";
+        return newMessage;
+    }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
